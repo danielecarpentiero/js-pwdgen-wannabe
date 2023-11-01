531a9eb0ca520a4d5bdbba05cafced4d1bd07eb6
Daniele Carpentiero <carpentierodaniele@gmail.com>
2023-11-01 11:53:18 +0100

    fix: single quotes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 "use strict";
 
-const elementName = document.getElementById('name');
-const elementSurname = document.getElementById('surname');
-const elementFavColor = document.getElementById('favColor');
-const elementFavNumber = document.getElementById('favNumber');
+const elementName = document.getElementById("name");
+const elementSurname = document.getElementById("surname");
+const elementFavColor = document.getElementById("favColor");
+const elementFavNumber = document.getElementById("favNumber");
 
 elementName.innerHTML = prompt("What's your name?");
 elementSurname.innerHTML = prompt("What's your surname?");
