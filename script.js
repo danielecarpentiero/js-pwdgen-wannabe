f2d9e1d07e956b3d8abf4c143ed1aca7023f2ef8
Daniele Carpentiero <carpentierodaniele@gmail.com>
2023-11-01 11:50:15 +0100

    feat: user infos prompt

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const elementName = document.getElementById('name');
+const elementSurname = document.getElementById('surname');
+const elementFavColor = document.getElementById('favColor');
+const elementFavNumber = document.getElementById('favNumber');
+
+elementName.innerHTML = prompt("What's your name?");
+elementSurname.innerHTML = prompt("What's your surname?");
+elementFavColor.innerHTML = prompt("What's your favorite color?");
+elementFavNumber.innerHTML = Math.floor((Math.random()*50)+1);
+
+console.log($`elementName` + $`elementSurname` + $`elementFavColor` + $`elementFavNumber`);
